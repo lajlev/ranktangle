4e99e2373fc1be342a45a13081c0f3f46693d386
Michael Lajlev <lajlev@gmail.com>
2013-03-09 22:48:26 +0100

    initial commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ranktangle::Application.config.secret_token = '58b3e736da0bcbb6179a38172d94235189d66914136061e2348f02d0066cb1c96b696ef84b62360ec0bde6b279401ea5acd9e8dd82388711c502e0e1441cd798'
